b270ac47b9d5d92c03383d56100d579120cca632
emssurya <emssurya56@gmail.com>
2026-02-12 11:18:06 +0530

    Add files via upload

---
+++ Five Numbers.R
@@ -0,0 +1,39 @@
+subset_df <-subset(mtcars,subset = cyl == 4)
+subset_df2  <-subset(mtcars,subset = cyl == 6)
+subset_df3  <-subset(mtcars,subset = cyl == 8)
+
+cat ("\n cars with 4 cylinders \n")
+cat ("-------------------------\n")
+print(subset_df)
+
+cat("\n five number summary of mpg values\n")
+cat("------------------------------------\n")
+
+
+print(fivenum (subset_df3$mpg))
+
+cat("\n cars with 6 cylinders\n")
+
+cat("------------------------\n")
+print(subset_df2)
+
+cat("\n five number summary of mpg values\n")
+
+cat("------------------------------------\n")
+
+print(fivenum(subset_df2$mpg))
+
+cat("\n cars with 8 cylinders\n")
+
+cat("------------------------\n")
+
+print(subset_df3)
+cat("\n five number summary of mpg values\n")
+
+cat("------------------------------------\n")
+print(fivenum (subset_df3$mpg))
+boxplot(mpg ~ cyl,
+        data = mtcars,
+        xlab = "number of cylinders",
+        ylab = "Miles per Gallon",
+        main = "Mileage Data")
